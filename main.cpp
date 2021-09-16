3413d8d8745aeead0943be8e6dea8b4beac5d3a4
Kirill Stepankov <fpm.stepanko@bsu.by>
2021-09-16 11:46:38 +0300

    Minor changes are made

--- main.cpp
+++ main.cpp
@@ -19,13 +19,12 @@ int main(){
     const double standart_result = 1/sqrt(1+x); //результат при помощи стандартных функций
 
     double part = 1; // очередное слагаемое 
-    int i = 1, var = 1;
+    int i = 1;
     double result=0;
-    while(abs(part) >= epsilon){
-        part = part*i/(i+1)*x;
-        result = result + pow(-1, var)*part;
+    while(fabs(part) >= epsilon){
+        part = -part*i/(i+1)*x;
+        result += part;
         i+=2;
-        var++;
     }
 
     result++;
